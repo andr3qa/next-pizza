7e1d2f8863428c5f399ea22ebe6efd137cea00a4
andr3qa <andr3qaa@gmail.com>
2025-10-02 21:55:39 +0400

    Configured the prisma

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,15 @@
-import { NextResponse } from 'next/server';
-import { prisma } from '../../../../prisma/prisma-client';
+import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
   const users = await prisma.user.findMany();
 
   return NextResponse.json(users);
+}
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+  const user = await prisma.user.create({ data });
+
+  return NextResponse.json(user);
 }
---
+++ src/lib/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@/generated/prisma';
+
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends({
+    query: {
+      async $allOperations({ operation, model, args, query }) {
+        return query(args);
+      },
+    },
+  });
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
+
+export default prisma;
